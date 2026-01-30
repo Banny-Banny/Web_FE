aad2c1295dcf15424334fdab88fa757c91387762
박초롱 <rong9835@naver.com>
2026-01-30 18:32:20 +0900

    feat: 프로필 하위 캡슐 보관함 페이지 라우트 및 페이지 추가

---
+++ src/app/(main)/profile/capsules/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * 캡슐보관함 페이지
+ * 마이페이지에서 "캡슐" 클릭 시 진입
+ */
+
+'use client';
+
+import { CapsuleStorage } from '@/components/CapsuleStorage';
+
+export default function ProfileCapsulesPage() {
+  return <CapsuleStorage />;
+}
