5057c9782181d8cf7e90d30eec060b4f3811ec19
박초롱 <rong9835@naver.com>
2026-01-31 22:07:24 +0900

    feat: 결제 내역 페이지 추가

---
+++ src/app/(main)/payment-history/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * Payment History Page
+ *
+ * @description
+ * - 결제 내역 페이지
+ * - GNB 표시 (Main Layout 적용)
+ */
+
+import { PaymentHistory } from '@/components/payment-history';
+
+export default function PaymentHistoryPage() {
+  return <PaymentHistory />;
+}
